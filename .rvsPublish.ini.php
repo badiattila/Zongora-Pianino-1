91f1d1d4a123e09f8b32adb30628319788959847
Attila Badi <attila.badi@gmail.com>
2020-11-26 20:42:00 +0000

    Procfile added for Heroku

---
+++ .rvsPublish.ini.php
@@ -0,0 +1,10 @@
+;<?php die("Eat dust"); ?>
+project_id=8edb249b592d16ec679b4147a307d134
+home_user="/home/bandland"
+publish_path="/home/bandland/public_html/zongora-pianino.hu"
+publish_url="http://zongora-pianino.hu"
+component_path="../../"
+publish_lang_file="hungarian-utf-8"
+folderExists=1
+fileOption=0
+isResponsive=0
