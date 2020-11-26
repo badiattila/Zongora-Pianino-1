f69dd91782fb3fbd465210a37d1086c7d4bd12b5
Attila Badi <attila.badi@gmail.com>
2020-11-26 20:15:14 +0000

    initial

---
+++ rvsMasterTemplate.php
@@ -0,0 +1,168 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="hu" lang="hu">
+<head>
+<title>Zongora, pianínó bérlés 200.- Ft.-tól</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="zongora bérlés, pianínó bérlés, zongora győr, zongora budapest, pianínó győr, pianínó budapest" />
+<meta name="description" content="Zongora és pianínó bérlés országosan verhetetlen áron!" />
+<meta name="generator" content="RVGlobalsoft" />
+<meta name="progid" content="Version: 4.99 PRO; Project name: zongora-pianino; Project id: 8edb249b592d16ec679b4147a307d134; Template Name: pro_21-915-2_red_floral_1; Published date: September 15, 2012, 12:23 26 (GMT +02:00" />
+ 
+<link rel="stylesheet" href="style.css" type="text/css" />
+<link rel="stylesheet" href="http://www.zongora-pianino.hu/Georgia.css" type="text/css" />
+<link rel="stylesheet" href="http://www.zongora-pianino.hu/Navigator.css" type="text/css" />
+<link rel="stylesheet" href="http://www.zongora-pianino.hu/pathway.css" type="text/css" />
+
+<!-- END RVS Jquery include -->
+
+<script  type="text/javascript" src="http://www.zongora-pianino.hu/rvsincludefile/rvsheadpage.js"></script>
+
+
+<script type="text/javascript" src="http://www.zongora-pianino.hu/rvsincludefile/rvsnavigator.js"></script>
+<script type="text/javascript" src="http://www.zongora-pianino.hu/js/publishNavigator/layersmenu-library.js"></script>
+<script type="text/javascript" src="http://www.zongora-pianino.hu/js/publishNavigator/layersmenu.js"></script>
+
+<script  type="text/javascript" src="http://www.zongora-pianino.hu/rvsincludefile/rvscustomopenwindow.js"></script>
+
+</head>
+
+<body>
+<table cellpadding="0" cellspacing="0" id="rv_top_adjust_width_0">
+	<tr>
+		<td align="left" valign="top"  class="bgtop"> 
+			<!-- START LOGO -->
+			<div style="position: absolute;">
+				<div id="Layer1" style="position:relative; left:330px; top:15px; width:120; height:60; text-align:center; z-index:1; overflow:visible; white-space:nowrap;"><!-- START LOGO --><!-- END LOGO --></div>
+			</div>
+			<div style="position: absolute;">
+				<div id="Layer2" style="position:relative; left:330px; top:85px; width:auto; height:auto; text-align:left; z-index:2; overflow:visible; white-space:nowrap;" class="company"><div>&nbsp;Piano Studio Kft.</div></div>
+			</div>
+			<div style="position: absolute;">
+				<div id="Layer3" style="position:relative; left:330px; top:110px; width:auto; height:auto; text-align:left; z-index:3; overflow:visible; white-space:nowrap;" class="slogan"><div>&nbsp;Napi 200.-Ft.-tól öné lehet egy zongora vagy pianínó</div></div>
+			</div>
+			<!-- END LOGO -->	
+			<table cellpadding="0" cellspacing="0" width="100%">
+				<tr>
+					<td align="left" valign="top"><img src="images/img_01.jpg" alt="" width="201" height="235" /></td>
+					<td align="left" valign="top" width="99%"><img src="images/img_02.jpg" alt="" width="207" height="235" /></td>
+					<td align="left" valign="top"><img src="images/img_03.jpg" alt="" width="207" height="235" /></td>
+					<td align="left" valign="top"><img src="images/img_04.jpg" alt="" width="165" height="235" /></td>
+				</tr>
+			</table>	
+		</td>
+	</tr>
+	<tr>
+		<td align="left" valign="top" >
+			<table cellpadding="0" cellspacing="0" width="100%">
+				 <tr>
+					<td rowspan="2"><img src="images/spacer.gif" alt="" width="35" height="1" /></td>
+					<td align="left" valign="top" class="lr"><img src="images/l_01.jpg" alt="" width="8" height="192" /></td>
+					<td rowspan="2" align="left" valign="top"  class="colorbody">
+						<table cellpadding="0" cellspacing="0" >
+							<tr>
+								<td class="mutop"><img src="images/spacer.gif" alt="" width="117" height="1" /></td>
+								<td class="mutop" valign="top" align="right"><img src="images/mu_01.jpg" alt="" width="12" height="27" /></td>
+							</tr>
+							<tr>
+								<td  align="left" valign="top" class="menu"><div class="navigator">
+<ul class="navigator">
+<li><a href="index.php"  class="normal"  target="_self" ><span>Zongora és Pianínó bérlés</span></a></li>
+<li><a href="Hangszereink.php"  class="normal"  target="_self" ><span>Hangszereink</span></a></li>
+<li><a href="%C3%9Czleti-aj%C3%A1nlat.php"  class="normal"  target="_self" ><span>Üzleti ajánlat</span></a></li>
+<li><a href="C%C3%A9g%C3%BCkr%C5%91l.php"  class="normal"  target="_self" ><span>Cégükről</span></a></li>
+</ul>
+</div>
+
+</td>
+								<td class="menu"></td>
+							</tr>
+							<tr>
+								<td class="menu"></td>
+								<td class="menu" valign="top" align="right"><img src="images/mu_05.jpg" alt="" width="12" height="10" /></td>
+							</tr>
+						</table>
+					</td>
+					<td align="left" valign="top" rowspan="2" width="99%" bgcolor="#e8e7e8">
+						<table cellpadding="0" cellspacing="0" width="100%">
+							 	
+
+							<!-- Begin PATHWAY and ICON -->
+							<tr>
+								<td class="magin">
+									<table cellpadding="0" cellspacing="0" width="100%">
+										<tr>
+											<!-- Begin PATHWAY -->
+											<td align="left" width="99%"><?php include("/home/bandland/public_html/zongora-pianino.hu/rvsincludefile/pathway_7c91cd8ffd0aab5cfb2c4b2d3ebc2f5c.html"); ?></td>
+											<!-- End PATHWAY -->
+											<!-- Begin ICON -->
+											<td align="right"><?php include("/home/bandland/public_html/zongora-pianino.hu/rvsincludefile/icon_7c91cd8ffd0aab5cfb2c4b2d3ebc2f5c.html"); ?></td>
+											<!-- End ICON -->
+										</tr>
+									</table>								
+								</td>
+							</tr>
+							<!-- End PATHWAY and ICON -->
+							<tr>
+								<td align="left" valign="top">
+									<table cellpadding="0" cellspacing="0" width="100%">
+										<tr>
+											<td align="left" valign="top" class="magin" id="rv_adjust_width_214">
+<?php $rvsComponent = $_GET['componentname'];
+
+echo" <div align=\"center\">Sorry, $rvsComponent have maintenance mode</div>"; ?>
+ 										    </td>
+											 
+
+										</tr>
+									</table>								
+								</td>
+							</tr>
+							<!-- Begin FOOTER -->
+							<tr>
+								<td align="center" class="magin"></td>
+							</tr>
+							<!-- End FOOTER -->
+							 
+
+							<tr>
+								<td align="center" valign="bottom">
+									<table cellpadding="0" cellspacing="0">
+										<tr>
+											<td align="center" valign="bottom" class="marginpw"></td>
+											<td width="8"></td>
+											<td align="center" valign="bottom" class="marginpw"></td>
+										</tr>
+									</table>													
+								</td>
+							</tr>
+					  </table>
+					</td>
+					<td align="right" valign="top" class="lr"><img src="images/r_01.jpg" alt="" width="7" height="17" /></td>
+					<td rowspan="2"><img src="images/spacer.gif" alt="" width="35" height="1" /></td>
+			  </tr>
+				  <tr>
+					<td align="left" valign="bottom" class="lr"><img src="images/l_02.jpg" alt="" width="8" height="52" /></td>
+					<td align="right" valign="bottom" class="lr"><img src="images/r_02.jpg" alt="" width="7" height="52" /></td>
+				  </tr>
+			</table>
+		</td>
+	</tr>
+	<tr>
+		<td align="left" valign="bottom">
+			<table cellpadding="0" cellspacing="0" width="100%">
+				<tr>
+					<td align="left" valign="top"><img src="images/img_05.jpg" alt="" width="43" height="71" /></td>
+					<td align="left" valign="top"><img src="images/img_06.jpg" alt="" width="159" height="71" /></td>
+					<td><img src="images/img_07.jpg" alt="" width="207" height="71" /></td>
+					<td width="99%" class="bottom"></td>
+					<td><img src="images/img_08.jpg" alt="" width="207" height="71" /></td>
+					<td><img src="images/img_09.jpg" alt="" width="124" height="71" /></td>
+					<td><img src="images/img_10.jpg" alt="" width="42" height="71" /></td>
+				</tr>
+			</table>		
+		</td>
+	</tr>
+</table>
+</body>
+</html>
